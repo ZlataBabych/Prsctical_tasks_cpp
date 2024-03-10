dde59a39f8be52343d6ac70f98d9b879abbe1020
Zlata Babych <126006978+ZlataBabych@users.noreply.github.com>
2024-03-10 16:13:42 +0200

    169.Порахуйте кількість одиниць у довільному числі.

--- Prsctical_tasks.cpp
+++ Prsctical_tasks.cpp
@@ -2,8 +2,36 @@
 //
 
 #include <iostream>
+#include<string>
+using namespace std;
+
+
+//169.Порахуйте кількість одиниць у довільному числі.
+int count_units(int number) {
+    int count = 0;
+    int i = 0;
+
+    string str = to_string(number);
+
+    char ch = str[0];
+
+   while (ch != '\0') {
+        ch = str[i];
+        cout << ch << " ";
+        if (ch == '1')
+            ++count;
+        ++i;
+    }
+
+    return count;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    //
+    int number = 0;
+    cin >> number;
+    cout << endl << count_units(number) << endl;
+
+    return 0;
 }
