df778aa4b5807a97cf9b76e3579411995fc1a71c
Zlata Babych <126006978+ZlataBabych@users.noreply.github.com>
2024-04-11 23:49:06 +0300

    Deque

--- Prsctical_tasks.cpp
+++ Prsctical_tasks.cpp
@@ -10,7 +10,20 @@
 
 using namespace std;
 
+const float PI_F = 3.14f;
 
+class Circle {
+public:
+    Circle(float r) {
+        if (r < 0)
+            throw std::exception("Radius < 0");
+        R = r;
+    }
+    float getArea() { return PI_F * R * R; };
+    float getPerimeter() { return PI_F * R * 2; };
+private:
+    float R;
+};
 
 
 
--- practical_tasks.h
+++ practical_tasks.h
@@ -8,6 +8,201 @@
 #include <memory>
 #include <string>
 
+// Deque from GitHub
+template <class T>
+class deque {
+private:
+    T* arr;
+    int front;
+    int rear;
+    int size;
+    int curr_size;
+
+public:
+
+    deque()
+    {
+        arr = new T[1];
+        front = -1;
+        rear = 0;
+        size = 0;
+        curr_size = 0;
+    }
+
+    deque(int size, T x)
+    {
+        arr = new T[size];
+        front = -1;
+        rear = 0;
+        curr_size = 0;
+        this->size = size;
+        for (int i = 0;i < size;i++)
+            push_back(x);
+    }
+    bool empty()
+    {
+        if (front == -1)
+            return true;
+        else return false;
+    }
+    bool isFull()
+    {
+        if (front == 0 && rear == size - 1)
+            return true;
+        else if (front == rear + 1)
+            return true;
+        else return false;
+    }
+    void push_back(T data)
+    {
+
+        if (isFull())
+        {
+            cout << " Overflow\n " << endl;
+            return;
+        }
+        if (rear == size - 1)
+            rear = 0;
+        else if (front == -1)
+        {
+            front = 0;
+            rear = 0;
+        }
+        else ++rear;
+        arr[rear] = data;
+        ++curr_size;
+    }
+
+    void pop_back()
+    {
+        if (empty())
+        {
+            cout << " Underflow\n";
+            return;
+        }
+        if (rear == 0)
+            rear = size - 1;
+        else if (front == rear)
+        {
+            front = -1;
+            rear = -1;
+        }
+        else --rear;
+        --curr_size;
+    }
+
+    void push_front(T data)
+    {
+        if (isFull())
+        {
+            cout << "Overflow\n" << endl;
+            return;
+        }
+        if (front == 0)
+            front = size - 1;
+        else if (front == -1)
+        {
+            front = 0;
+            rear = 0;
+        }
+
+        else --front;
+        arr[front] = data;
+        ++curr_size;
+    }
+
+    void pop_front()
+    {
+        if (empty())
+        {
+            cout << "Queue Underflow\n" << endl;
+            return;
+        }
+        if (front == rear)
+        {
+            front = -1;
+            rear = -1;
+        }
+        else front == (front + 1) % size;
+        --curr_size;
+    }
+
+    int get_size()
+    {
+        return ((size - front + rear) % size);
+    }
+
+    T back()
+    {
+        return arr[size - 1];
+    }
+
+    T get_front()
+    {
+        return arr[1];
+    }
+
+    void resize(int x, T d)
+    {
+        if (curr_size == x)
+            return;
+        else if (curr_size > x)
+        {
+            int i = curr_size - x;
+            while (i--)
+                pop_front();
+        }
+
+        else
+        {
+            int i = x - curr_size;
+            while (i--)
+                push_front(d);
+            size = (size - front + rear) % size;
+        }
+
+    }
+
+    T D(int n)
+    {
+        return arr[n];
+    }
+
+    void clear()
+    {
+        front = -1;
+        rear = 0;
+        size = 0;
+        curr_size = 0;
+    }
+
+
+
+    void display()
+
+    {
+        if (front == -1)
+        {
+            cout << "Empty\n";
+            return;
+        }
+        cout << "\n Elements are: ";
+        if (rear >= front)
+        {
+            for (int i = front;i <= rear;i++)
+                cout << arr[i] << endl;
+        }
+        else
+        {
+            for (int i = front;i < size;i++)
+                cout << arr[i];
+            for (int i = 0;i <= rear;i++)
+                cout << arr[i] << endl;
+        }
+    }
+};
+
+
 //169. Calculate the number of units in any number.
 int count_units(int number);
 
